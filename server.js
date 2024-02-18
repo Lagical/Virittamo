66ff27919a43fefcd495c75a847472f72b2682b8
Lagical <70884073+Lagical@users.noreply.github.com>
2024-02-18 20:59:16 +0200

    added .env file

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const FormDataRoutes = require('./server/routes/FormDataRoutes.js');
 const cors = require('cors');
+require('dotenv').config()
 
 
 const app = express(); // Creating an instance of Express application
 
 // Retrieving MongoDB URI from environment variables
-const mongodbUri = process.env.MONGODB_URI;
+const mongodbUri = process.env.REACT_APP_MONGODB_URI;
 
 // Enable CORS for all origins
 app.use(cors());
@@ -19,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Connect to MongoDB database
-mongoose.connect('mongodb+srv://groupiron:N8GuIiLAT8fd7OXj@cluster0.zjyrlee.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(mongodbUri, {
 })
 .then(() => console.log('Connected to MongoDB')) // Log success message if connected to MongoDB
 .catch(err => console.error('Error connecting to MongoDB:', err)); // Log error if connection fails
